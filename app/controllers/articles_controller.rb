ae0769c323ec3778c7eafdc797d4f9228e0b3c09
kenderb <kenderbolivar@gmail.com>
2020-11-17 19:50:51 -0500

    Create the model for the articles table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
   end
 
 end
