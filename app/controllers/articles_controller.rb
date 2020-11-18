61875709a0f939fe218589df9f7894d833426da7
kenderb <kenderbolivar@gmail.com>
2020-11-17 20:22:07 -0500

    Add private article_params method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
   end
 
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
