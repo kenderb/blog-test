ffcb663a099233294a6fc2c7cc5f34303359b4e2
kenderb <kenderbolivar@gmail.com>
2020-11-17 21:14:40 -0500

    Create basic validation and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,31 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def show
